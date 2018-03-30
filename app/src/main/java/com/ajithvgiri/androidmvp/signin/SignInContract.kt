3d21d3d9fb89da3dad2912c652ff461a1058f15d
ajithvgiri <mail.ajithvgiri@gmail.com>
2018-03-31 00:42:07 +0530

    SignIn MVP initial commit

---
+++ app/src/main/java/com/ajithvgiri/androidmvp/signin/SignInContract.kt
@@ -0,0 +1,11 @@
+package com.ajithvgiri.androidmvp.signin
+
+/**
+ * Defines the contract between the View MainActivity and the SignInPresenter.
+ */
+interface SignInContract {
+
+    interface MVPview {}
+
+    interface Presenter {}
+}
